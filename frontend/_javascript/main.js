12b231819eda40d032062bdc36ba3a4ab953c799
bdbaraban <375@holbertonschool.com>
2019-05-24 16:44:34 -0700

    Update D3 rendering to work on real live data

--- frontend/_javascript/main.js
+++ frontend/_javascript/main.js
@@ -1,11 +1,11 @@
-const barChart = () => {
+const barChart = (rawData) => {
   // sample language usage data. 'raw language name': byte_count_integer
-  const rawData = {
+ /*  const rawData = {
     C: 12345,
     Python: 23456,
     JavaScript: 5432,
     Rust: 876,
-  };
+  }; */
 
   // create user-friendly sample array of objects
   const sample = Object.keys(rawData).map(k => Object({language: k, bytes: rawData[k]}));
@@ -64,8 +64,8 @@ const login = () => {
 
   verify().then(verify().then(() => {
     $.get('/api/v1/me', (data) => {
-      $('div.card-content').append('<div class="bar-chart" style="width:1000px; height:600px; margin: auto; background-color: #3A99AD;"><svg style="width: 100%; height: 100%;"/></div>');
-      barChart();
+      $('div.card-content').append('<div class="bar-chart" style="width: 750px; height: 600px; margin: auto; background-color: #3A99AD;"><svg style="width: 100%; height: 100%;"/></div>');
+      barChart(data.lang_metric);
       $('button.loginout').text('Logout');
       $('button.loginout').attr('id', 'logout');
       $('p.card-header-title').text(`${data.g_login}`);
